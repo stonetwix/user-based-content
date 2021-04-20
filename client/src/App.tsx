b6dcd969742bcf38f0129f001b6200f1da257ad1
msmalinosterberg <msmalinosterberg@gmail.com>
2021-04-20 15:23:43 +0200

    added successmessage

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
 import Header from './components/Header';
 import LogInView from './components/login/LogInView';
+import RegisterSuccess from "./components/login/RegisterSuccess";
 import ScrollToTop from './components/ScrollToTop';
 import PostsListUser from './components/userAdminPosts/PostsList';
 
@@ -16,7 +17,9 @@ function App() {
         <LogInView /> 
         </Switch>
       <ScrollToTop />
-     
+      <Switch>
+      <RegisterSuccess />
+      </Switch>
       <Switch>
         {/* <Route path="/" component={DetailView} /> */}
         <Route path="/" component={PostsListUser} />
--- client/src/components/login/RegisterSuccess.tsx
+++ client/src/components/login/RegisterSuccess.tsx
@@ -1,1 +1,38 @@
-import React from 'react';
+import { Result, Button, Row, Col } from 'antd';
+import { CSSProperties } from 'react';
+import { Link } from 'react-router-dom';
+
+function SuccessMessage() {
+    return (
+        <Row style={containerStyle}>
+            <Col span={24} style={colStyle}>
+                <Result
+                    status="success"
+                    title="You successfully registered an account on Traveler"
+                    extra={[
+                    <Link to='/'>
+                        {/*OBS! Lägg in rätt länk */ }
+                        <Button type="primary" key="console">Log in</Button>
+                    </Link>
+                    ]}
+                />
+              
+            </Col>
+        </Row>
+    ) 
+}
+
+export default SuccessMessage; 
+
+const containerStyle: CSSProperties = {
+    margin: 'auto'
+}
+
+const colStyle: CSSProperties = {
+    display: 'flex',
+    flexDirection: 'column',
+    marginTop: '10rem',
+    marginBottom: '5rem',
+    justifyContent: 'center',
+    alignItems: 'center',
+}
