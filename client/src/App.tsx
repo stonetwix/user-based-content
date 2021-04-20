9062685636aeac15ea68c19a9df3e1bd41b407a0
Moa Stenqvist <moa@stonetwig.se>
2021-04-20 12:11:22 +0200

    Add basic content for detail view.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,14 +2,16 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
+import DetailView from './components/detailview/DetailView';
+import ScrollToTop from './components/ScrollToTop';
 
 function App() {
   return (
     <Router>
-      {/* <ScrollToTop />
-      <Header /> */}
+      <ScrollToTop />
+      {/* <Header /> */}
       <Switch>
-        {/* <Route path="/product/:id" component={ProductDetails} /> */}
+        <Route path="/" component={DetailView} />
       </Switch>
   </Router>
   );
