4db87d43ecfefa4357ce81f792aa6cf77c89e7de
Moa Stenqvist <moa@stonetwig.se>
2021-04-20 15:23:08 +0200

    merge main into edit-post.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
+import DetailView from "./components/detailview/DetailView";
 import Header from './components/Header';
 import LogInView from './components/login/LogInView';
 import ScrollToTop from './components/ScrollToTop';
@@ -7,24 +8,17 @@ import PostsListUser from './components/userAdminPosts/PostsList';
 import EditPost from './components/userAdminPosts/UserEditPost';
 
 function App() {
-  return (
-    
+  return (   
     <Router>
-  
-      <Header /> 
-      <Switch>
-        {/* <Route path="/product/:id" component={ProductDetails} /> */}
-        <LogInView /> 
-        </Switch>
+      {/* <Header />  */}
       <ScrollToTop />
-     
       <Switch>
+        {/* <Route path="/" component={LogInView} /> */}
         {/* <Route path="/" component={DetailView} /> */}
         {/* <Route path="/" component={PostsListUser} /> */}
         <Route path="/" component={EditPost} />
       </Switch>
   </Router>
- 
   );
 }
 
