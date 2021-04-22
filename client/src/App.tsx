6e11b97f8899778848ae7abb87797698a6c47d69
msmalinosterberg <msmalinosterberg@gmail.com>
2021-04-22 11:40:45 +0200

    routing working

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -22,7 +22,7 @@ function App() {
         <Route exact path="/" component={StartPageView} />
         <Route path="/post/:id" component={PostDetail} />
         <Route path="/login" component={LogInView} />
-        <Route path="/user" component={PostsListUser} />
+        <Route exact path="/user" component={PostsListUser} />
         <Route path="/user/add-new-post" component={UserAddNewPost} />
         <Route path="/edit-post/:id" component={EditPost} />
         <Route path="/registersuccess" component={RegisterSuccess} />
