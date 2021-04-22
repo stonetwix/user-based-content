fce96ebf4eabad321b026a1a13b1e0dfb16d2407
Moa Stenqvist <moa@stonetwig.se>
2021-04-22 14:28:34 +0200

    Posts endpoints read from/write to Mongo DB.

--- server/routers/posts.router.js
+++ server/routers/posts.router.js
@@ -4,43 +4,66 @@ const fs = require('fs');
 const PostModel = require('../models/posts.model');
 
 //Endpoints
-router.get('/api/posts', (req, res) => {
-    res.status(200).json(readPosts());
+router.get('/api/posts', async (req, res) => {
+    const posts = await PostModel.find({});
+    res.status(200).json(posts);
+    //res.status(200).json(readPosts());
 });
 
-router.get('/api/posts/:id', (req, res) => {
-    const posts = readPosts();
-    const post = posts.find(p => p.id === Number(req.params.id));
-    if (!post) {
+router.get('/api/posts/:id', async (req, res) => {
+    try {
+        const post = await PostModel.findById(req.params.id);    
+        res.status(200).json(post);
+    } catch (error) {
         res.status(404).json({ error: 'Post not available' });
     }
-    res.status(200).json(post);
+    
+    // const posts = readPosts();
+    // const post = posts.find(p => p.id === Number(req.params.id));
+    // if (!post) {
+    //     res.status(404).json({ error: 'Post not available' });
+    // }
+    // res.status(200).json(post);
 });
 
-router.post('/api/posts', (req, res) => {
-    const posts = readPosts();
-    const newPost = req.body;
-    newPost.id = Math.max(...posts.map((item) => item.id)) + 1;
-    posts.push(newPost);
-    writePosts(posts);
+router.post('/api/posts', async (req, res) => {
+    const newPost = await PostModel.create(req.body);
+    // const posts = readPosts();
+    // const newPost = req.body;
+    // newPost.id = Math.max(...posts.map((item) => item.id)) + 1;
+    // posts.push(newPost);
+    // writePosts(posts);
     res.status(201).json(newPost);
 });
 
-router.put('/api/posts/:id', (req, res) => {
-    let posts = readPosts();
-    const post = req.body;
-    const id = Number(req.params.id);
-    post.id = id;
-    posts = posts.map((item) => item.id === id ? post : item);
-    writePosts(posts);
-    res.status(200).json(req.body);
+router.put('/api/posts/:id', async (req, res) => {
+    try {
+        const post = await PostModel.findById(req.params.id).updateOne(req.body);
+        res.status(200).json(post);
+    } catch (error) {
+        res.status(404).json({ error: 'Post not available' });
+    }
+    // let posts = readPosts();
+    // const post = req.body;
+    // const id = Number(req.params.id);
+    // post.id = id;
+    // posts = posts.map((item) => item.id === id ? post : item);
+    // writePosts(posts);
+    // res.status(200).json(req.body);
 });
 
-router.delete('/api/posts/:id', (req, res) => {
-    let posts = readPosts();
-    posts = posts.filter((item) => item.id !== Number(req.params.id));
-    writePosts(posts);
-    res.status(204).json({});
+router.delete('/api/posts/:id', async (req, res) => {
+    try {
+        await PostModel.findById(req.params.id).deleteOne(req.body);
+        res.status(204).json({});
+        
+    } catch (error) {
+        res.status(404).json({ error: 'Post not available' });
+    }
+    // let posts = readPosts();
+    // posts = posts.filter((item) => item.id !== Number(req.params.id));
+    // writePosts(posts);
+    // res.status(204).json({});
 });
 
 module.exports = router;
