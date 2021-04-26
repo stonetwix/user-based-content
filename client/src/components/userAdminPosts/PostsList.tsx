7e82b61d750fb59eafa59223d022ef2b4435810d
Moa Stenqvist <moa@stonetwig.se>
2021-04-26 18:23:57 +0200

    Add username on page when logged in, add conditional rendering on user menu item in sider.

--- client/src/components/userAdminPosts/PostsList.tsx
+++ client/src/components/userAdminPosts/PostsList.tsx
@@ -1,9 +1,10 @@
-import { Component, CSSProperties } from 'react';
+import { Component, ContextType, CSSProperties } from 'react';
 import { Link, RouteComponentProps } from 'react-router-dom';
 import { Layout, Button, List, Avatar, message } from 'antd';
 import { PlusCircleOutlined, FormOutlined, DeleteOutlined } from '@ant-design/icons';
 import SiderMenu from './SiderMenu';
 import { User } from '../admin/AdminUserList';
+import { UserContext } from '../context';
 
 const { Content } = Layout;
 
@@ -20,22 +21,23 @@ interface Props extends RouteComponentProps<{ _id: string }> {}
  
 interface State {
     posts?: Post[];
-    //user?: User;
+    user?: User;
 }
 
 const successDelete = () => {
     message.success('The product has been deleted', 3);
 };
 class PostsListUser extends Component <Props, State> {
+    context!: ContextType<typeof UserContext>
+    static contextType = UserContext;
 
     state: State ={
         posts: [],
-        //user: undefined,
+        user: undefined,
     }
     
     async componentDidMount() {
         const posts = await getPosts();
-        //const user = await getUser
         this.setState({ posts: posts });
     }
 
@@ -45,58 +47,64 @@ class PostsListUser extends Component <Props, State> {
         this.setState({ posts: posts });
     }
     
-    render () {
+    render() {
         return (
-            <Layout style={{ background: '#fff' }}>
-                <SiderMenu />
-                <Content style={{ margin: '8rem', background: '#fff' }}>
-                    <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
-                        <h1 style={{ marginBottom: '2rem' }}>Welcome Username{}</h1>
-                    <Link to={'/user/add-new-post'}> 
-                        <Button type="primary" 
-                            icon={<PlusCircleOutlined />} 
-                            style={{ marginBottom: '4rem' }} 
-                            onClick={() => console.log('new post clicked')}
-                        > 
-                            Create New Post 
-                        </Button> 
-                    </Link>
-                        <List
-                            itemLayout="horizontal"
-                            dataSource={this.state.posts}
-                            renderItem={item => (
-                            <List.Item actions={[
-                                <Link to={'/edit-post/' + item._id}>  
+            <UserContext.Consumer>
+            {({ username }) => {
+            return (
+                <Layout style={{ background: '#fff' }}>
+                    <SiderMenu />
+                    <Content style={{ margin: '8rem', background: '#fff' }}>
+                        <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
+                            <h1 style={{ marginBottom: '2rem' }}>Welcome {username}</h1>
+                        <Link to={'/user/add-new-post'}> 
+                            <Button type="primary" 
+                                icon={<PlusCircleOutlined />} 
+                                style={{ marginBottom: '4rem' }} 
+                                onClick={() => console.log('new post clicked')}
+                            > 
+                                Create New Post 
+                            </Button> 
+                        </Link>
+                            <List
+                                itemLayout="horizontal"
+                                dataSource={this.state.posts}
+                                renderItem={item => (
+                                <List.Item actions={[
+                                    <Link to={'/edit-post/' + item._id}>  
+                                        <Button 
+                                        key="edit-post" 
+                                        onClick={() => console.log('edit-clicked')}
+                                        style={editStyle}
+                                        icon={<FormOutlined />}
+                                        >
+                                            edit
+                                        </Button>
+                                    </Link>, 
                                     <Button 
-                                    key="edit-post" 
-                                    onClick={() => console.log('edit-clicked')}
-                                    style={editStyle}
-                                    icon={<FormOutlined />}
+                                        key="delete-post" 
+                                        onClick={() => {this.handleDelete(item._id); successDelete();}}                                 
+                                        style={deleteStyle}
+                                        icon={<DeleteOutlined />}
                                     >
-                                        edit
-                                    </Button>
-                                </Link>, 
-                                <Button 
-                                    key="delete-post" 
-                                    onClick={() => {this.handleDelete(item._id); successDelete();}}                                 
-                                    style={deleteStyle}
-                                    icon={<DeleteOutlined />}
+                                        delete
+                                    </Button>]}
                                 >
-                                    delete
-                                </Button>]}
-                            >
-                            <List.Item.Meta
-                                avatar={<Avatar src={item.imageUrl} style={{ width: '4rem', height: '4rem' }}/>}
-                                title={item.title}
-                                description={item.text.substring(0, 35) + '...'}
+                                <List.Item.Meta
+                                    avatar={<Avatar src={item.imageUrl} style={{ width: '4rem', height: '4rem' }}/>}
+                                    title={item.title}
+                                    description={item.text.substring(0, 35) + '...'}
+                                />
+                                </List.Item>
+                                )}
                             />
-                            </List.Item>
-                            )}
-                        />
-                    </div>
-                </Content>
-            </Layout>
-        ); 
+                        </div>
+                    </Content>
+                </Layout>
+                )
+            }}
+            </UserContext.Consumer>
+        )
     }    
 }
 
